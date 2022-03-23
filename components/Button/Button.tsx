b4b6cb7e789dede743d2efc93da0df81dc70c159
Fer Wicker <fer.wicker@gmail.com>
2022-03-23 20:46:47 +1100

    get custom params for prompts and pass to prompt page

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,14 +5,25 @@ import css from './Button.module.scss';
 type Props = {
     className?: string;
     children: string | React.ReactNode;
+    value?: string;
+    onClick?: Function;
 }
 
 const Button: React.FunctionComponent<Props> = ({
     className,
     children,
+    value,
+    onClick,
 }) => {
+
+    const handleClick = () => {
+        typeof onClick === 'function' 
+            ? onClick(value)
+            : null
+    }
+
     return (
-        <button className={[css.button, className].join(' ')}>{children}</button>
+        <button className={[css.button, className].join(' ')} onClick={handleClick}>{children}</button>
     )
 }
 
---
+++ pages/prompts/custom/index.tsx
@@ -0,0 +1,227 @@
+import * as React from 'react';
+import type { NextPage } from 'next'
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+
+import css from '../index.module.scss'
+
+import HeadMeta from '../../../components/HeadMeta'
+import smallLogo from '../../../public/images/hor-logo.png';
+import MenuButton from '../../../components/MenuButton';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import Button from '../../../components/Button';
+
+library.add(faShuffle);
+
+const CustomPrompt: NextPage = () => {
+    const [season, setSeason] = React.useState<any>({});
+    const [shuffle, setShuffle] = React.useState<string[]>([]);
+
+    const router = useRouter();
+
+    React.useEffect(() => {
+        console.log(shuffle);
+    }, [shuffle]);
+
+    const handleGenerateClick = () => {
+        if (shuffle.length === 0) {
+            alert('Select at least one parameter');
+        } else {
+            router.push({
+                pathname: '/prompts/custom/prompt',
+                query: {
+                    ...season,
+                    shuffle: shuffle,
+                },
+            })
+        }
+    };
+
+    const handleSeasonClick = (current: string) => {
+        if (season.season && season.season === current) {
+            setSeason({});
+        } else {
+            setSeason({
+                season: current,
+            });
+        }
+    };
+
+    const handleParamClick = (current: string) => {
+        const tempArr = [...shuffle];
+        const index = tempArr.indexOf(current) 
+
+        if (index > -1) { // if already in array, remove
+            tempArr.splice(index, 1);
+            setShuffle(tempArr);
+        } else if (shuffle.length > 4) { // if max reached, alert
+            alert('Select maximum 5!');
+        } else {
+            tempArr.push(current);
+            setShuffle(tempArr);
+        }
+    };
+
+    return (
+        <>
+        <HeadMeta
+            title="Hashtag Awesome Nails | Random Prompt"
+            description="Set custom preferences for your nail prompts"
+        />
+        <Link href="/prompts">
+            <a className={css.menuButtonLink}>
+                <MenuButton>Back</MenuButton>
+            </a>
+        </Link>
+        <div className={css.smallLogoContainer}>
+            <Image
+                src={smallLogo} 
+                alt="Hashtag Awesome Prompts logo" 
+                width="896px"
+                height="98px"
+                className={css.logo} />
+        </div>
+        <div className={[css.prefContainer, css.seasons].join(' ')}>
+            <p>
+                <span className={css.headline}>Season </span>
+                <span className={css.smallHeadline}>{'(optional)'}</span>
+            </p>
+            <div className={css.row}>
+                <Button 
+                    className={[css.buttonHalfWidth, season.season === 'spring' ? css.buttonSelected : ''].join(' ')}
+                    value={'spring'}
+                    onClick={handleSeasonClick}
+                >
+                    Spring
+                </Button>
+                <Button 
+                    className={[css.buttonHalfWidth, season.season === 'summer' ? css.buttonSelected : ''].join(' ')}
+                    value={'summer'}
+                    onClick={handleSeasonClick}
+                >
+                    Summer
+                </Button>
+            </div>
+            <div className={css.row}>
+            <Button 
+                    className={[css.buttonHalfWidth, season.season === 'autumn' ? css.buttonSelected : ''].join(' ')}
+                    value={'autumn'}
+                    onClick={handleSeasonClick}
+                >
+                    Autumn
+                </Button>
+                <Button 
+                    className={[css.buttonHalfWidth, season.season === 'winter' ? css.buttonSelected : ''].join(' ')}
+                    value={'winter'}
+                    onClick={handleSeasonClick}
+                >
+                    Winter
+                </Button>
+            </div>
+        </div>
+        <div className={[css.prefContainer].join(' ')}>
+            <p><span className={css.headline}>Pick up to 5</span></p>
+            <div className={[css.row, css.wrapRow].join(' ')}>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple, 
+                        shuffle.indexOf('length') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'length'}
+                    onClick={handleParamClick}
+                >
+                    Length
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('shape') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'shape'}
+                    onClick={handleParamClick}
+                >
+                    Shape
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('colour1') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'colour1'}
+                    onClick={handleParamClick}
+                >
+                    Colour 1
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('colour2') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'colour2'}
+                    onClick={handleParamClick}
+                >
+                    Colour 2
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('art') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'art'}
+                    onClick={handleParamClick}
+                >
+                    Art
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('glitter') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'glitter'}
+                    onClick={handleParamClick}
+                >
+                    Glitter
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('vibe') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'vibe'}
+                    onClick={handleParamClick}
+                >
+                    Vibe
+                </Button>
+                <Button 
+                    className={[
+                        css.buttonHalfWidth, 
+                        css.buttonMultiple,
+                        shuffle.indexOf('extras') > -1 ? css.buttonSelected : '',
+                    ].join(' ')}
+                    value={'extras'}
+                    onClick={handleParamClick}
+                >
+                    Extras
+                </Button>
+            </div>
+        </div>
+                <button onClick={handleGenerateClick} className={[css.shuffleButton, css.generateButton].join(' ')}>
+                    <span className={css.generateText}>Generate</span>
+                    <FontAwesomeIcon icon="shuffle" color="#ec1c24" size="2x" />
+                </button>
+        </>
+    )
+}
+
+export default CustomPrompt;

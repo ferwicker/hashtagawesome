f694be82c162d979ca115fab449bed7bf19f8733
Fer Wicker <fer.wicker@gmail.com>
2022-03-29 19:53:16 +1100

    check for correct icon

--- components/DownloadButton/DownloadButton.tsx
+++ components/DownloadButton/DownloadButton.tsx
@@ -1,49 +1,86 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
+import html2canvas from 'html2canvas';
+import { isMobile } from 'react-device-detect';
 
 import css from './DownloadButton.module.scss';
 
+import PromptValue from '../PromptValue';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import { faDownload, faShare} from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-library.add(faCamera);
+library.add(faDownload, faShare);
+
+type Prompt = {
+    title: string,
+    value: string,
+}
 
 type Props = {
-    handleScreenshot?: Function;
+    promptValues?: Prompt[];
 }
 
 const DownloadButton: React.FunctionComponent<Props> = ({
-    handleScreenshot,
+    promptValues,
 }) => {
+    const [imageHidden, setImageHidden] = React.useState(true);
+    const [isMobileDevice, setIsMobileDevice] = React.useState(isMobile);
 
-    // const ref = React.createRef(null);
-    /* const [image, takeScreenShot] = useScreenshot({
-        type: "image/jpeg",
-        quality: 1.0
-    });
+    // detect device
 
-    const download = (image, { name = "img", extension = "jpg" } = {}) => {
-        const a = document.createElement("a");
-        a.href = image;
-        a.download = createFileName(extension, name);
-        a.click();
-      };
+    // generate image to be downloaded
 
-    const downloadScreenshot = () => takeScreenShot(ref.current).then(download); */
+    // function for desktop
 
+    React.useEffect(() => {
+        !imageHidden && Download();
+    }, [imageHidden])
+
+    const handleDownload = () => {
+        setImageHidden(false);
+    }
+
+    const Download = () => {
+        let div:HTMLElement = document.getElementById('screenshot') !;
+        !imageHidden && html2canvas(div).then(canvas => {
+            const link = document.createElement('a');
+            link.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            link.download = 'Download.jpg';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            setImageHidden(true);
+        })
+    }
+
+    //function for mobile
+
+    // click handler
     const handleClick = () => {
-       if (typeof handleScreenshot === 'function') {
-            handleScreenshot();
-        }
+       console.log(isMobileDevice);
     }
 
     return (
+        <>
         <button 
             className={css.button}
             onClick={handleClick}
         >
-            <FontAwesomeIcon icon="camera" color="#ec1c24" size="2x"  />
+            <FontAwesomeIcon icon={isMobileDevice ? "share" : "download"} color="#fff" size="2x"  />
         </button>
+        <div id={'screenshot'} className={[css.canvasDiv, imageHidden ? css.hide : null].join(' ')}>
+            <div className={css.promptValues}>
+                {promptValues && promptValues.length > 0  && promptValues.map((el, i) => {
+                    return (
+                        <PromptValue key={i} title={el.title} value={el.value}  />
+                    )
+                })}
+            </div>
+            <p className={css.shareP}>Share your creations with #hashtagawesomeprompts</p>
+        </div>
+        </>
     )
 }
 
--- pages/prompts/random.tsx
+++ pages/prompts/random.tsx
@@ -35,7 +35,6 @@ const RandomPrompt: NextPage = () => {
 
     const [promptValues, setPromptValues] = React.useState<Prompt[]>([]);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const [imageHidden, setImageHidden] = React.useState(true);
 
     const toggleMenuOpen = () => {
         let tempOpen = isMenuOpen;
@@ -75,34 +74,13 @@ const RandomPrompt: NextPage = () => {
         setPromptValues(tempArr);
     }
 
-    const Download = () => {
-        let div:HTMLElement = document.getElementById('screenshot') !;
-        !imageHidden && html2canvas(div).then(canvas => {
-            const link = document.createElement('a');
-            link.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-            link.download = 'Download.jpg';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-            setImageHidden(true);
-        })
-    }
-
-    React.useEffect(() => {
-        !imageHidden && Download();
-    }, [imageHidden])
-
-    const handleScreenshot = () => {
-        setImageHidden(false);
-    }
-
     return (
         <>
         <HeadMeta
             title="Hashtag Awesome Nails | Random Prompt"
             description="Get a random nail prompt to inspire your creativity"
         />
-        <DownloadButton handleScreenshot={handleScreenshot} />
+        <DownloadButton />
         <div className={css.menuButtonContainer}>
             <Link href="/prompts">
                 <a className={[css.halfWidth, css.buttonLeft].join(' ')}>
@@ -132,16 +110,6 @@ const RandomPrompt: NextPage = () => {
         <button className={css.shuffleButton} onClick={() => { getPrompt(optionsObject) }} >
             <FontAwesomeIcon icon="shuffle" color="#ec1c24" size="3x" />
         </button>
-        <div id={'screenshot'} className={[css.canvasDiv, imageHidden ? css.hide : null].join(' ')}>
-            <div className={css.promptValues}>
-                {promptValues.length > 0  && promptValues.map((el, i) => {
-                    return (
-                        <PromptValue key={i} title={el.title} value={el.value}  />
-                    )
-                })}
-            </div>
-            <p className={css.shareP}>Share your creations with #hashtagawesomeprompts</p>
-        </div>
         </>
     )
 }
